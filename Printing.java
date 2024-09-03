f894cee326fa76f3d058c915c22f729c1aae06ad
TinaD1906 <dotina87@gmail.com>
2024-09-03 10:06:42 -0700

    1.1 Assignment

---
+++ Printing.java
@@ -0,0 +1,15 @@
+public class Printing
+{
+    public static void main(String [] args)
+    {
+        //int, double, boolean, String 
+        int myAge = 999; // int is for whole numbers; integers
+        double price = 4.99;
+
+        boolean isGood; // variable declaration
+        isGood = false; // variable initialization
+
+        String name = "Tina Do"; //set name to "Tina Do"
+        System.out.print("Hello my name is Tina Do");
+    }
+}
